854dca44444eb236e39ff3d1f78c4f3070d30649
Pawan Aichra <aichrapawan@gmail.com>
2021-10-10 09:18:11 +0530

    lambda timeout

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -4,12 +4,12 @@ from utils import *
 
 def lambda_handler(event, context):
     hmm = HMM()
-    stock_prices = get_stock_prices('SBIN', '2017-01-01', '2017-04-01')
+    stock_prices = get_stock_prices('SBIN', '2021-01-01', '2021-02-01')
     price_movement = get_price_movements(stock_prices)
-    optimal_states = hmm.get_optimal_states(price_movement)
+    optimal_states = 2 #lambda timeout issue
     prediction = hmm.predict(price_movement, optimal_states)
     result = ""
-    if prediction==1:
+    if prediction == 1:
         result = "You should buy stock."
     else:
         result = "Sell stock if you have."
